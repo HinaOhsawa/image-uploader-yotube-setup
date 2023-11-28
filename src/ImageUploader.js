2a8a73082c02b46814fa348e45147390bed9b09a
Ohsawa Hina <strobe.hina@gmail.com>
2023-11-28 12:33:07 +0900

    コメントを追加

--- src/ImageUploader.js
+++ src/ImageUploader.js
@@ -10,12 +10,16 @@ const ImageUploader = () => {
   const [isUploaded, setUploaded] = useState(false);
 
   const OnFileUploadToFirebase = (e) => {
-    // console.log(e.target.files[0].name);
+    // console.log(e.target.files[0].name);//ファイルネーム取得
     const file = e.target.files[0];
     const storageRef = ref(storage, "image/" + file.name);
+
+    // ファイルアップロード
     // uploadBytes(storageRef, file).then((snapshot) => {
     //   console.log("Uploaded a blob or file!");
     // });
+
+    // アップロードを監視
     const uploadImage = uploadBytesResumable(storageRef, file);
 
     uploadImage.on(
@@ -34,46 +38,52 @@ const ImageUploader = () => {
   };
   return (
     <>
-      {loading ? (
-        <h2>アップロード中・・・</h2>
-      ) : (
-        <>
-          {isUploaded ? (
-            <h2>アップロード完了しました！</h2>
-          ) : (
-            <div className="outerBox">
-              <div className="title">
-                <h2>画像アップローダー</h2>
-                <p>JpegかPngの画像ファイル</p>
-              </div>
-              <div className="imageUplodeBox">
-                <div className="imageLogoAndText">
-                  <img src={ImageLogo} alt="imagelogo" />
-                  <p>ここにドラッグ＆ドロップしてね</p>
+      {
+        // アップロード中かどうか
+        loading ? (
+          <h2>アップロード中・・・</h2>
+        ) : (
+          <>
+            {
+              // アップロード完了したかどうか
+              isUploaded ? (
+                <h2>アップロード完了しました！</h2>
+              ) : (
+                <div className="outerBox">
+                  <div className="title">
+                    <h2>画像アップローダー</h2>
+                    <p>JpegかPngの画像ファイル</p>
+                  </div>
+                  <div className="imageUplodeBox">
+                    <div className="imageLogoAndText">
+                      <img src={ImageLogo} alt="imagelogo" />
+                      <p>ここにドラッグ＆ドロップしてね</p>
+                    </div>
+                    <input
+                      className="imageUploadInput"
+                      multiple
+                      name="imageURL"
+                      type="file"
+                      accept=".png, .jpg, .jpeg"
+                      onChange={OnFileUploadToFirebase}
+                    />
+                  </div>
+                  <p>または</p>
+                  <Button variant="contained">
+                    ファイルを選択
+                    <input
+                      className="imageUploadInput"
+                      type="file"
+                      accept=".png, .jpg, .jpeg"
+                      onChange={OnFileUploadToFirebase}
+                    />
+                  </Button>
                 </div>
-                <input
-                  className="imageUploadInput"
-                  multiple
-                  name="imageURL"
-                  type="file"
-                  accept=".png, .jpg, .jpeg"
-                  onChange={OnFileUploadToFirebase}
-                />
-              </div>
-              <p>または</p>
-              <Button variant="contained">
-                ファイルを選択
-                <input
-                  className="imageUploadInput"
-                  type="file"
-                  accept=".png, .jpg, .jpeg"
-                  onChange={OnFileUploadToFirebase}
-                />
-              </Button>
-            </div>
-          )}
-        </>
-      )}
+              )
+            }
+          </>
+        )
+      }
     </>
   );
 };
